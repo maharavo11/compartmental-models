b9eb2d70a54fa16b7a6d7eeb677dbabb138520b0
maharavo11 <maharavo@aims.ac.za>
2025-05-19 17:54:45 +0300

    Update data.py

--- src/data.py
+++ src/data.py
@@ -3,3 +3,7 @@ resource_urls= [
     "https://raw.githubusercontent.com/MLGlobalHealth/StatML4PopHealth/main/practicals/day4/practical5/data/flu_data.csv",
     "https://raw.githubusercontent.com/MLGlobalHealth/StatML4PopHealth/main/practicals/day4/practical5/data/incidence.csv",
      ]
+
+import subprocess
+for url in resource_urls:
+   subprocess.run(["curl", "-O", url], check=True)
