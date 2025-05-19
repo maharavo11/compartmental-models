878e2ee077177c4e97f67ed35cbe2024791209a8
maharavo11 <maharavo@aims.ac.za>
2025-05-19 20:08:09 +0200

    Update sir-model-fit-inference.py

--- src/sir-model-fit-inference.py
+++ src/sir-model-fit-inference.py
@@ -1,3 +1,13 @@
+
+import pandas as pd
+df = pd.read_csv('flu_data.csv')
+
+# Convert the dates to datetime format
+df.Date = pd.to_datetime(df.date)
+
+# Use date as index
+df = df.set_index("date")
+
 # Set the population size
 N = 763
 
