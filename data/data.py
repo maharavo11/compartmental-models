fc9ab14de56809e83b46ccf1b515d79cd1f6a0e8
maharavo11 <maharavo@aims.ac.za>
2025-05-19 20:06:30 +0200

    Update data.py

--- data/data.py
+++ data/data.py
@@ -7,3 +7,12 @@ resource_urls= [
 import subprocess
 for url in resource_urls:
    subprocess.run(["curl", "-O", url], check=True)
+
+import pandas as pd
+df = pd.read_csv('flu_data.csv')
+
+# Convert the dates to datetime format
+df.Date = pd.to_datetime(df.date)
+
+# Use date as index
+df = df.set_index("date")
