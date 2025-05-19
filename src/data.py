e07de72bb12ac40859e67f3cb7164938f396647d
maharavo11 <maharavo@aims.ac.za>
2025-05-19 18:30:40 +0300

    Delete src/data.py

--- src/data.py
+++
@@ -1,9 +0,0 @@
-# The data used are in these links
-resource_urls= [
-    "https://raw.githubusercontent.com/MLGlobalHealth/StatML4PopHealth/main/practicals/day4/practical5/data/flu_data.csv",
-    "https://raw.githubusercontent.com/MLGlobalHealth/StatML4PopHealth/main/practicals/day4/practical5/data/incidence.csv",
-     ]
-
-import subprocess
-for url in resource_urls:
-   subprocess.run(["curl", "-O", url], check=True)
