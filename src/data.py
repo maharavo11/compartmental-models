618be76dccde4326c5d81b30fc3b0dd0526f4164
maharavo11 <maharavo@aims.ac.za>
2025-05-19 17:39:57 +0300

    Create data.py

---
+++ src/data.py
@@ -0,0 +1,5 @@
+# The data used are in these links
+resource_urls= [
+    "https://raw.githubusercontent.com/MLGlobalHealth/StatML4PopHealth/main/practicals/day4/practical5/data/flu_data.csv",
+    "https://raw.githubusercontent.com/MLGlobalHealth/StatML4PopHealth/main/practicals/day4/practical5/data/incidence.csv",
+     ]
